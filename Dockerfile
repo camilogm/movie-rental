624fde610b14bade19fcb555100f42ed9e93d3cb
Camilo Gonzalez <camilogit123@gmail.com>
2021-01-11 08:39:32 -0600

    🔧 Add procfile for heroku deployment

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:12.18.2 as builder
 WORKDIR /app
 ADD package.json /app/package.json
 RUN npm config set registry http://registry.npmjs.org
-RUN npm install
+RUN npm install --production
 ADD . /app
 EXPOSE 3000
 CMD ["npm", "run", "start"]
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 export default () => ({
   ENVIROMENT: process.env.NODE_ENV,
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: Number(process.env.PORT) || 3000,
   JWT_SECRET: process.env.jwt,
   EMAIL_TRANSPORT: process.env.email_transport,
   DB_CONFIGURATION: {
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
