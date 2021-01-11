766f2eca356b452c84fa882deef027751818144d
Camilo Gonzalez <camilogit123@gmail.com>
2021-01-11 09:36:24 -0600

    ♻️ Refactor for server port injection

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/api/v1/', app, document);
+  const PORT = Number(process.env.PORT) || 3000;
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
